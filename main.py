37f4c0c5430af45f422a1eda8a584db2aef4b21b
BengalWolf77 <sgoodfellow@gmail.com>
2025-09-26 20:04:39 -0400

    added rotation and movement methods to player

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ def main():
                 return
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
