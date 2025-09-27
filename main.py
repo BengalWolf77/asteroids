c9a59f947a66c923cce71768c44ae688680ac36f
BengalWolf77 <sgoodfellow@gmail.com>
2025-09-27 19:26:56 -0400

    added asteroids and asteroidfield

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ os.environ["SDL_AUDIODRIVER"] = "dummy"
 import pygame
 from constants import *
 from player import *
+from asteroid import * 
+from asteroidfield import *
 
 def main():
     pygame.init()
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updatable,drawable)
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
 
     while True:
         for event in pygame.event.get():
