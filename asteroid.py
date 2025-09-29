7976a776264f68b456d28217a79de71b899f9aa8
BengalWolf77 <sgoodfellow@gmail.com>
2025-09-29 13:35:41 -0400

    bullets now kill asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from circleshape import *
 from constants import *
 
@@ -10,7 +11,20 @@ class Asteroid(CircleShape):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
-        #new_x = self.x + self.velocity * dt
-        #new_y = self.y + self.velocity * dt
-
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        angle = random.uniform(20,50)
+        new_vect = pygame.math.Vector2.rotate(self.velocity, angle) * 1.2
+        new_vect2 = pygame.math.Vector2.rotate(self.velocity, -angle) * 1.2
+
+        child1 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+        child1.velocity = new_vect
+
+        child2 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+        child2.velocity = new_vect2
+        
+        
--- main.py
+++ main.py
@@ -13,11 +13,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-    Shot.containers = (updatable, drawable)
+    Shot.containers = (updatable, drawable, shots)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
@@ -40,6 +41,13 @@ def main():
                 print("Game over!")
                 return
 
+        for obj in shots:
+            for obj2 in asteroids:
+                if obj2.collides_with(obj):
+                    obj.kill()
+                    obj2.split()
+                    break
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
